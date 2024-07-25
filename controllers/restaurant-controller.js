193b26f0dc0866f586cddbcf7e1b41f1ec5030d9
syh053 <syh053@gmail.com>
2024-07-25 14:04:09 +0800

    build front desk router

---
+++ controllers/restaurant-controller.js
@@ -0,0 +1,7 @@
+const restaurantController = {
+  getRestaurnats: (req, res) => {
+    return res.render('restaurants')
+  }
+}
+
+module.exports = restaurantController
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// 載入 restaurant-controller
+const restController = require('../controllers/restaurant-controller')
+
+router.get('/restaurants', restController.getRestaurnats)
+
+// 若匹配不到路由，最後進到從導向路由
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
