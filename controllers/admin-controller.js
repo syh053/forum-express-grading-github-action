74947a5f8b86920b8a5e8fab14ea4e8ca6b7f8be
syh053 <syh053@gmail.com>
2024-07-31 22:55:59 +0800

    modify admin restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,14 @@
+const { Restaurant } = require('../db/models')
+
 const adminController = {
-  getRestaurant: (req, res) => {
-    return res.render('admin/restaurants')
+  getRestaurant: (req, res, next) => {
+    Restaurant.findAll({ raw: true })
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => {
+        err.name = '資料庫'
+        err.message = '資料庫錯誤!!'
+        next(err)
+      })
   }
 }
 
