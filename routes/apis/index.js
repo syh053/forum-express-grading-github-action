4b899e3039b36772f29d0f454a3bcad0f7915249
syh053 <syh053@gmail.com>
2024-09-29 21:53:32 +0800

    adjust restaurants/feeds router position

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -41,11 +41,11 @@ router.get('/restaurants', auth, restController.getRestaurants)
 
 router.get('/restaurants/top', auth, restController.getTopRestaurants) // 查看前10名被收藏的餐廳訊息
 
+router.get('/restaurants/feeds', auth, restController.getFeeds) // 查看最新 10 比餐廳、評論路由
+
 router.get('/restaurants/:id', auth, restController.getRestaurant) // 查看單筆餐廳訊息
 
 router.get('/restaurants/:id/dashboard', auth, restController.getDashboard) // 查看單筆餐廳活動數據
-
-router.get('/restaurants/feeds', auth, restController.getFeeds) // 查看最新 10 比餐廳、評論路由
 
 router.post('/comments', auth, commentController.postComment)
 
