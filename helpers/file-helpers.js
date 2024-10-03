55e25c04101e0f2b3f8c71d6369880642ba69548
syh053 <syh053@gmail.com>
2024-10-03 21:30:04 +0800

    check temp, upload folder exist

--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -1,4 +1,10 @@
 const fs = require('fs')
+const path = require('path')
+
+const dirPath = path.join(__dirname, '..', 'upload') // 定義 temp 檔案路徑
+
+// 檢查 temp 資料夾是否存在，若不存在則建立 temp 資料夾
+if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath)
 
 const localFileHandler = file => { // file 是 multer 處理完的檔案
   return new Promise((resolve, reject) => {
--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,5 +1,13 @@
 const multer = require('multer')
 
+const fs = require('fs')
+const path = require('path')
+
+const dirPath = path.join(__dirname, '..', 'temp')
+
+// 檢查資料夾是否存在，若不存在則建立資料夾
+if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath)
+
 const upload = multer({ dest: 'temp/' })
 
 module.exports = upload
